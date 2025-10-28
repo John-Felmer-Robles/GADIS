5f7a999bda9576bbb15b96f0ec32829adb48d252
OneInTwoJackets <robles.johnfelmer2005@gmail.com>
2025-10-28 20:54:40 +0800

    added firebase file

--- god/src/server.js
+++ god/src/server.js
@@ -1,24 +1,17 @@
-const mysql = require("mysql2/promise");
-async function connectDB() {
-  const db = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "express_db",
-  });
-  console.log("Connected to MySQL database");
-  return db;
-}
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const express = require("express");
-const connectDB = require("./db");
+const firebaseConfig = {
+  apiKey: "AIzaSyAQkgQZOvFJ4bTU9ys5pS0cg8QVAbCpRDE",
+  authDomain: "gadis-8a320.firebaseapp.com",
+  databaseURL: "https://gadis-8a320-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gadis-8a320",
+  storageBucket: "gadis-8a320.firebasestorage.app",
+  messagingSenderId: "306615637491",
+  appId: "1:306615637491:web:aaba8925f0792d1adeee86",
+  measurementId: "G-T752R0LCDM"
+};
 
-const app = express();
-app.use(express.json());
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-module.exports = connectDB;
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
